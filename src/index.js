eeb2b7e13ee03ac2c34338ebeb72bcd7a03478e7
Adrian Manczak <manczakadrian@gmail.com>
2023-11-20 19:13:23 +0100

    timer_update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './styles/normalize.scss';
+import './styles/global.scss';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
 );
-
